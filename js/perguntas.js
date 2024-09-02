84f164f42a4b51af746c2d76b8c087a3948d5d45
kiikodochaves <129853267+kiikodochaves@users.noreply.github.com>
2024-09-02 08:11:08 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,176 @@
+export const perguntas = [
+    {
+        enunciado: "Qual é o propósito principal de uma festa de aniversário?",
+        alternativas: [
+            {
+                texto: "a) Comemorar o aniversário de alguém",
+                afirmacao: [
+                    "O propósito principal de uma festa de aniversário é celebrar o aniversário da pessoa homenageada, marcando a data de seu nascimento com uma comemoração especial..",
+                    "."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "b) Realizar um inventário de escritório",
+                afirmacao: [
+                    "Realizar um inventário de escritório não é relacionado ao propósito de uma festa de aniversário, que é a celebração pessoal.",
+                    "."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é um item típico encontrado em uma festa de aniversário?",
+        alternativas: [
+            {
+                texto: "a) Bolo de aniversário",
+                afirmacao: [
+                    " O bolo de aniversário é um item tradicional e simbólico em festas de aniversário, frequentemente decorado e usado para cantar Parabéns.",
+                    ".",
+                    ""
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "b) Ferramentas de jardinagem",
+                afirmacao: [
+                    "Ferramentas de jardinagem não têm relação com a celebração de uma festa de aniversário e não são tipicamente encontradas em tais eventos" ,
+                    "",
+                    "."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é um costume comum durante a festa de aniversário?",
+        alternativas: [
+            {
+                texto: "a) Cantar parabéns ",
+                afirmacao: [
+                    "Cantar a música de Parabéns é uma tradição amplamente aceita em festas de aniversário, simbolizando a celebração e felicitações para o aniversariante",
+
+                    "",
+                    ""
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "b) Organizar um leilão de caridade",
+                afirmacao: [
+                    " Leilões de caridade não são uma prática comum em festas de aniversário; eles são eventos separados com objetivos específicos de arrecadação de fundos.",
+                    ".",
+                    ""
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é uma atividade popular em festas de aniversário infantis?",
+        alternativas: [
+            {
+                texto: "a) Jogos e brincadeiras",
+                afirmacao: [
+                    " Em festas de aniversário infantis, jogos e brincadeiras são organizados para entreter as crianças e tornar a celebração mais divertida.",
+                    "."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "b) Sessão de fotos para um portfólio profissional",
+                afirmacao: [
+                    "Sessões de fotos para portfólios profissionais não são típicas de festas de aniversário infantis; essas festas são mais focadas em atividades recreativas para as crianças.",
+                    ""
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é um presente típico para um aniversário?",
+        alternativas: [
+            {
+                texto: "a) Um presente pessoal ou algo que a pessoa gosta .",
+                afirmacao: [
+                    " Presentes pessoais e itens que a pessoa homenageada apreciaria são comuns em aniversários, pois mostram consideração pelos gostos e interesses da pessoa.",
+                    ""
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "b) Um novo conjunto de panelas de cozinha",
+                afirmacao: [
+                    "Panelas de cozinha não são geralmente consideradas presentes típicos para aniversários, a menos que se trate de uma ocasião específica como um casamento.",
+                    ""
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a finalidade de uma festa surpresa?",
+        alternativas: [
+            {
+                texto: "a) Celebrar um evento sem que a pessoa homenageada saiba antecipadamente ",
+                afirmacao: [
+                    " Uma festa surpresa é planejada para que o aniversariante não saiba da celebração até o momento em que a surpresa é revelada, proporcionando um elemento de surpresa e entusiasmo.",
+                    ""
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "b) Organizar uma reunião de planejamento de projeto",
+                afirmacao: [
+                    " Uma festa surpresa não é organizada para fins de planejamento de projetos de trabalho, que é um objetivo diferente e não relacionado ao conceito de uma festa surpresa.",
+                    ""
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é um elemento comum na decoração de festas de aniversário?",
+        alternativas: [
+            {
+                texto: "a) Balões e streamers",
+                afirmacao: [
+                    "Balões e streamers são usados frequentemente para decorar o ambiente em festas de aniversário, criando uma atmosfera festiva e alegre.",
+                    ""
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "b) b) Equipamentos de laboratório",
+
+                afirmacao: [
+                    " Equipamentos de laboratório não são usados como parte da decoração em festas de aniversário; eles são mais apropriados para ambientes científicos e acadêmicos.",
+                    ""
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é um costume associado à distribuição de lembrancinhas?",
+        alternativas: [
+            {
+                texto: "a) Oferecer pequenos presentes aos convidados como agradecimento ",
+                afirmacao: [
+                    "Lembrancinhas são pequenas recordações dadas aos convidados em agradecimento por sua presença, uma prática comum em festas de aniversário para mostrar apreço",
+
+                    ""
+                ],
+            },
+            {
+                texto: "b) Fornecer mapas de uma cidade para turistas",
+                afirmacao: [
+                    "Mapas de cidades para turistas não têm relação com a distribuição de lembrancinhas em festas de aniversário, que são pequenas lembranças pessoais para os convidados.",
+                    "",
+                    ""
+                ],
+            },
+        ]
+    }
+];
